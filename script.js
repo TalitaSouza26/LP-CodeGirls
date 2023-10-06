6439194992d96f1a617e45c81e9a418e3890d3c5
DevTalita <taa.jesus09@gmail.com>
2023-10-06 16:45:27 -0300

    Landing Page Flower.

---
+++ script.js
@@ -0,0 +1,14 @@
+let next = document.querySelector('.esquerda')
+let prev = document.querySelector('.direita')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
+
+
